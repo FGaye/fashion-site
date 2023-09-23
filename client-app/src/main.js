2e6336ac6a6f282d04637a19372c406ce4a8588f
FGaye <fg22126029@utg.edu.gm>
2023-09-23 15:41:12 +0000

    frontend folder structure

---
+++ client-app/src/main.js
@@ -0,0 +1,9 @@
+ 
+import { createApp } from 'vue'
+import {BootstrapVue} from 'bootstrap-vue'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App.vue'
+
+createApp(App).use(BootstrapVue).mount('#app')
