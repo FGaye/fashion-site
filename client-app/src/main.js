5eafb0afb7ce3a273e8c88ea1dcd72c85b03a1c3
FGaye <fg22126029@utg.edu.gm>
2023-09-23 15:48:12 +0000

    corrected bootstrap config

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -1,9 +1,8 @@
- 
+
 import { createApp } from 'vue'
-import {BootstrapVue} from 'bootstrap-vue'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
-createApp(App).use(BootstrapVue).mount('#app')
+createApp(App).mount('#app')
