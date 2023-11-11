bc289653ee30fcf892353a83c6dab4a2ce4a51d0
FGaye <fg22126029@utg.edu.gm>
2023-11-11 22:25:35 +0000

    hotfix

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import NavBar from './components/NavBar.vue';
 
 </script>
 
 <template>
     <div id="app">
        <header>
+ <NavBar />
+
          </header>
             <main>
             <router-view >
--- client-app/src/views/HomeView.vue
+++ client-app/src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
- <NavBar />
     <Carousel class="px-4" />
     <Cards />
     <Promotion />
@@ -8,6 +7,5 @@
 <script setup>
 import Cards from '../components/Cards.vue';
 import Carousel from '../components/Carousel.vue';
-import NavBar from '../components/NavBar.vue';
 import Promotion from '../components/Promotion.vue';
 </script>
